dd6d487ef1167c1b21d022a291d0fbcf561d093c
DocThomus <bonninthom2@gmail.com>
2018-06-29 14:58:22 +0200

    first commit

---
+++ src/main/java/fr/durandal/durandalback/DurandalBackApplication.java
@@ -0,0 +1,12 @@
+package fr.durandal.durandalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DurandalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DurandalBackApplication.class, args);
+	}
+}
