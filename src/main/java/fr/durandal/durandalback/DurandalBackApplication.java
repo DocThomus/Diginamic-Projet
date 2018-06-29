719a761582907036783a0909af0bc98d83870b6c
charles <charlesal@hotmail.fr>
2018-06-29 17:46:46 +0200

    Functional-ish

--- src/main/java/fr/durandal/durandalback/DurandalBackApplication.java
+++ src/main/java/fr/durandal/durandalback/DurandalBackApplication.java
@@ -1,8 +1,14 @@
 package fr.durandal.durandalback;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("fr.durandal.durandalback")
 @SpringBootApplication
 public class DurandalBackApplication {
 
---
+++ src/main/java/fr/durandal/durandalback/model/User.java
@@ -0,0 +1,5 @@
+package fr.durandal.durandalback.model;
+
+public class User {
+
+}
