edf115ab99c52b05f3c7300ebfa78009423d0b45
g_rey <gabdu74@gmail.com>
2018-07-11 15:04:11 +0200

    edit user account

--- src/main/java/fr/durandal/durandalback/user/AuthenticationController.java
+++ src/main/java/fr/durandal/durandalback/user/AuthenticationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +43,7 @@ public class AuthenticationController {
 	@PutMapping(value = "/updateUser" , consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus( HttpStatus.ACCEPTED)
 	public Visitor updateUser(@RequestBody Visitor v) {
+		System.out.println("UPDATE " + v);
 		String passwordNotHashed = v.getHashedPassword();
 		String passwordHashed = securityConfig.passwordEncoder().encode(passwordNotHashed);
 		v.setHashedPassword(passwordHashed);
@@ -49,4 +51,10 @@ public class AuthenticationController {
 		v.setHashedPassword(null);
 		return v;	
 	}
+	
+	@GetMapping(value="/getUser", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Visitor current(@RequestParam String email) {
+		return authDAO.getUserByEmail(email);
+	}
+	
 }
